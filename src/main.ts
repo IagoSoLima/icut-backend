d2f2086bfa04902e72aa3973f63f104fc5b9d882
Iago So Lima <iago.souza069@gmail.com>
2023-05-27 14:03:41 -0300

    chore: config logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { rateLimit } from 'express-rate-limit';
+import helmet from 'helmet';
+import { AppLogger } from '~/app.logger';
+import { AppModule } from '~/app.module';
+import { APP_PORT, APP_VERSION, APP_VERSION_PREFIX } from '~/app.vars';
+import { enableCors } from '~/cors.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  try {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(), {
+      logger: new AppLogger()
+    });
+    await app.listen(4000);
+    app.enableVersioning({
+      type: VersioningType.URI,
+      prefix: APP_VERSION_PREFIX,
+      defaultVersion: APP_VERSION
+    });
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        forbidUnknownValues: true
+      })
+    );
+
+    enableCors(app);
+    app.use(helmet());
+    app.use(rateLimit({ windowMs: 60 * 1000, max: 1000 }));
+
+    const port = (process.env.PORT ?? APP_PORT) as number;
+
+    await app.listen(port, () => {
+      Logger.verbose(`Icut listening at http://localhost:${port} 🙌 `, 'Main');
+    });
+  } catch (err) {}
 }
 bootstrap();
